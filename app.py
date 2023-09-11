dc0428329586ef2271daaf503e916230cb891ac1
189302081 <sanjilmahajani@gmail.com>
2023-09-11 15:18:30 +0530

    1st attempt to solve errors

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 from src.utils import load_object
 
-application=Flask(__name__)
+app=Flask(__name__)
 
-app=application
+#app=application
 
 ## Route for a home page
 
---
+++ app1.py
@@ -0,0 +1,13 @@
+from flask import Flask,request,render_template
+app1=Flask(__name__)
+
+@app1.route('/')
+def index():
+    return render_template('')  # Make sure the template name matches your file name exactly.
+
+if __name__ == '__main__':
+    app1.run()
+
+
+
+
